a4f70037a0e45f4a0e9787d607093af0d4e233f3
Gavin-JH <gavinjh@163.com>
2019-12-08 17:11:19 +0800

    init ropor

---
+++ src/test/java/com/gavin/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
