b43359748272f53eb70dbe619054a39f0d66293d
gavin-jh <gavinjh@163.com>
2019-12-08 19:23:36 +0800

    add 导航条

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ ES：社区https://elasticsearch.cn/explore
 创建KEY：https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys
 前端开发资料：https://v3.bootcss.com/
 视频资源https://www.bilibili.com/video/av62800055?p=6
+
+登录使用GitHub：https://developer.github.com/apps/#
 #工具
---
+++ src/main/java/com/gavin/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.gavin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Gavin on 2019/12/8.
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
